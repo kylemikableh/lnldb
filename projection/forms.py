bfa4a58ca8a7144368eac8e2cab36712943c8113
Kyle Mikolajczyk <kjmikolajczyk@wpi.edu>
2020-11-09 13:00:58 -0500

    Began to remove anything to do with projection liscensing in the DB. Removed fields to add liscense expiry or number and editing these fields

--- projection/forms.py
+++ projection/forms.py
@@ -26,9 +26,8 @@ class ProjectionistUpdateForm(forms.ModelForm):
         self.helper.layout = Layout(
 
             # 'pit_level',
-            'license_number',
-            HTML('<div class="pull-right"><a id="resetdate" class="btn btn-lg btn-warning">Reset Date</a></div>'),
-            Field('license_expiry', css_class="datepick"),
+            # 'license_number',
+            HTML(' '),
         )
         super(ProjectionistUpdateForm, self).__init__(*args, **kwargs)
 
@@ -46,8 +45,8 @@ class ProjectionistForm(forms.ModelForm):
         self.helper.layout = Layout(
             'user',
             # 'pit_level',
-            'license_number',
-            Field('license_expiry', css_class="datepick"),
+            # 'license_number',
+            #Field('license_expiry', css_class="datepick"),
             FormActions(
                 Submit('save', 'Save Changes'),
             )
